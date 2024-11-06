a14536c2fbb4a3f0cef7e65da546d8ad8c795db8
Sixtails1 <103711641+Sixtails1@users.noreply.github.com>
2024-11-05 22:25:33 -0700

    Added TailwindCSS

---
+++ src/entrypoints/popup/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Excalidraw in Canvas</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{ts,js,mjs,cjs,mts,cts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
